d0cc32a1a32e7ae85de3540bf0bc9451d9043b09
Thiagoxdbs <thiago-hiago201@hotmail.com>
2024-03-11 15:42:05 -0300

    Resultado final gerador de CPF

--- Aulas Udemy Otavio Miranda/Aula_103_Gerador_cpf.py
+++ Aulas Udemy Otavio Miranda/Aula_103_Gerador_cpf.py
@@ -1,10 +1,5 @@
 import random
 
-validacao = ''
-for gerador in range(9):
-    validacao += str(random.randint(0,9))
-
-
 def cpf(digito , quantidade_cpf):
     soma_cpf = 0
     for con_ in digito:
@@ -19,13 +14,14 @@ def cpf(digito , quantidade_cpf):
     valor = mult_div_10_11 if mult_div_10_11 <= 9 else 0
     return valor
 
-pri_char_ent = validacao[0]
-pri_char_ent_rep = pri_char_ent * len(validacao)
+for i in range(100):
+    validacao = ''
+    for gerador in range(9):
+        validacao += str(random.randint(0,9))
 
+    dig_1 = cpf(validacao[:9], 10)
+    dig_2 = cpf(validacao[:10], 11)
 
-dig_1 = cpf(validacao[:9], 10)
-dig_2 = cpf(validacao[:10], 11)
+    task_cpf_def = f'{validacao[:9]}{dig_1}{dig_2}'
 
-task_cpf_def = f'{validacao[:9]}{dig_1}{dig_2}'
-
-print(task_cpf_def)
+    print(task_cpf_def)
