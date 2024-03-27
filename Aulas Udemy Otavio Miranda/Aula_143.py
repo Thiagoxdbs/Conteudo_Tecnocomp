afaa212af4ff7080f5b5bbc278acb4805dcdc82b
Thiagoxdbs <thiago-hiago201@hotmail.com>
2024-03-27 16:51:52 -0300

    Aula 144

--- Aulas Udemy Otavio Miranda/Aula_143.py
+++ Aulas Udemy Otavio Miranda/Aula_143.py
@@ -1,15 +1,16 @@
 # Valores Truthy e Falsy, Tipos Mutáveis e Imutáveis
 # Mutáveis [] {} set()
 # Imutáveis () "" 0 0.0 None False range(0, 10)
-lista = []
-dicionario = {}
-conjunto = set()
-tupla = ()
-string = ''
-inteito = 0
-flutuante = 0.0
-nada = None
-falso = False
+
+lista = [],
+dicionario = {},
+conjunto = set(),
+tupla = (),
+string = '',
+inteito = 0,
+flutuante = 0.0,
+nada = None,
+falso = False,
 intervalo = range(0)
 
 
