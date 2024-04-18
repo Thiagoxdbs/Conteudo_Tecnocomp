5190fdb115a8f4322f8899632ae06b7b5dd25562
Thiagoxdbs <thiago-hiago201@hotmail.com>
2024-04-18 11:32:01 -0300

    Aula 165

--- Aulas Udemy Otavio Miranda/Aula_163.py
+++ Aulas Udemy Otavio Miranda/Aula_163.py
@@ -1,3 +1,5 @@
+from random import randint
+
 # Exercício - Adiando execução de funções
 def soma(x, y):
     return x + y
@@ -7,7 +9,7 @@ def multiplica(x, y):
     return x * y
 
 
-def criar_funcao(funcao, x):
+def criar_funcao(funcao, x): 
     def interna(y):
         return funcao(x, y)
     return interna
@@ -18,3 +20,4 @@ multiplica_por_dez = criar_funcao(multiplica, 10)
 
 print(soma_com_cinco(10))
 print(multiplica_por_dez(10))
+
